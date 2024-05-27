5007fd76b6ed825bb23005854a9e148fe61bac60
FinByz Tech Pvt. Ltd <info@finbyz.tech>
2024-05-27 16:42:48 +0530

    Update setup.py
    Removed the pip.req from setup.py file

--- setup.py
+++ setup.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 from setuptools import setup, find_packages
-from pip.req import parse_requirements
 import re, ast
 
 # get version from __version__ variable in ibt_call_center/__init__.py
