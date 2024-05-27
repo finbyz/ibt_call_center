b45f0323b229432d765090a0decb15fc6c6d03a4
FinByz Tech Pvt. Ltd <info@finbyz.tech>
2024-05-27 18:03:25 +0530

    Resetting setup.py
    Resetting Setup.py

--- setup.py
+++ setup.py
@@ -1,25 +1,28 @@
 # -*- coding: utf-8 -*-
 from setuptools import setup, find_packages
 import re, ast
+from pip._internal.req import parse_requirements
+from pip._internal.network.session import PipSession
 
-with open('requirements.txt') as f:
-	install_requires = f.read().strip().split('\n')
-
-# get version from __version__ variable in ibtcustom/__init__.py
+# get version from __version__ variable in ibt_call_center/__init__.py
 _version_re = re.compile(r'__version__\s+=\s+(.*)')
 
-with open('ibtcustom/__init__.py', 'rb') as f:
+with open('ibt_call_center/__init__.py', 'rb') as f:
     version = str(ast.literal_eval(_version_re.search(
         f.read().decode('utf-8')).group(1)))
 
+# Parsing requirements.txt using pip's internal API
+requirements = parse_requirements("requirements.txt", session=PipSession())
+
 setup(
-	name='ibtcustom',
-	version=version,
-	description='App to manage serverside customizations',
-	author='FinByz Tech Pvt Ltd',
-	author_email='info@finbyz.com',
-	packages=find_packages(),
-	zip_safe=False,
-	include_package_data=True,
-	install_requires=install_requires
+    name='ibt_call_center',
+    version=version,
+    description='Call Center',
+    author='IBT',
+    author_email='callcenteribt@gmail.com',
+    packages=find_packages(),
+    zip_safe=False,
+    include_package_data=True,
+    install_requires=[str(ir.requirement) for ir in requirements],
+    dependency_links=[str(ir.link.url) for ir in requirements if ir.link]
 )
